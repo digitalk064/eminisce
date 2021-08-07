68d098ddf235945204c34c917bacc4e03e2c9b07
digitalk064 <nguynkhanh.lv@gmail.com>
2021-08-07 18:10:27 +0700

    Heroku does not have crontab but leaving code for portablility.

--- eminisce/apps.py
+++ eminisce/apps.py
@@ -12,7 +12,7 @@ class EminisceAppConfig(AppConfig):
         # Don't run on local because Windows does not have cron
         if 'runserver' not in sys.argv and 'makemigrations' not in sys.argv and 'migrate' not in sys.argv and 'crontab' not in sys.argv: 
             print("Starting crontab jobs")
-            #subprocess.Popen([sys.executable, os.getcwd() + '/eminisce/start_background_tasks.py'], env=os.environ.copy(),)
-            process_tasks_cmd = "python manage.py crontab add"
-            process_tasks_args = shlex.split(process_tasks_cmd)
-            process_tasks_subprocess = subprocess.Popen(process_tasks_args)
+            subprocess.Popen([sys.executable, 'manage.py', 'crontab', 'add'], env=os.environ.copy(),)
+            # process_tasks_cmd = "python manage.py crontab add"
+            # process_tasks_args = shlex.split(process_tasks_cmd)
+            # process_tasks_subprocess = subprocess.Popen(process_tasks_args)
