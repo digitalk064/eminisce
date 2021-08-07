9d425b0946911c9df41471a1a5009107abe515e3
digitalk064 <nguynkhanh.lv@gmail.com>
2021-08-07 17:31:01 +0700

    Again

--- eminisce/apps.py
+++ eminisce/apps.py
@@ -1,13 +1,15 @@
 from django.apps import AppConfig
 
 import subprocess, os, sys
+import shlex
 
 class EminisceAppConfig(AppConfig):
     name = 'eminisce'
     verbose_name = "Eminisce"
     def ready(self):
-        if 'runserver' not in sys.argv and 'makemigrations' not in sys.argv and 'migrate' not in sys.argv: # Don't run on local because Windows does not have cron
+        if True: #if 'runserver' not in sys.argv and 'makemigrations' not in sys.argv and 'migrate' not in sys.argv: # Don't run on local because Windows does not have cron
             print("Starting crontab jobs")
             #subprocess.Popen([sys.executable, os.getcwd() + '/eminisce/start_background_tasks.py'], env=os.environ.copy(),)
             process_tasks_cmd = "python manage.py crontab add"
-            process_tasks_subprocess = subprocess.Popen(process_tasks_cmd, env=os.environ.copy())
+            process_tasks_args = shlex.split(process_tasks_cmd)
+            process_tasks_subprocess = subprocess.Popen(process_tasks_args)
