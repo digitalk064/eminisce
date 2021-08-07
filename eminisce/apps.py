8353fc8c576db654466cbf4edeb50d77e9c932f0
digitalk064 <nguynkhanh.lv@gmail.com>
2021-08-07 17:28:13 +0700

    Again

--- eminisce/apps.py
+++ eminisce/apps.py
@@ -9,5 +9,5 @@ class EminisceAppConfig(AppConfig):
         if 'runserver' not in sys.argv and 'makemigrations' not in sys.argv and 'migrate' not in sys.argv: # Don't run on local because Windows does not have cron
             print("Starting crontab jobs")
             #subprocess.Popen([sys.executable, os.getcwd() + '/eminisce/start_background_tasks.py'], env=os.environ.copy(),)
-            process_tasks_cmd = sys.executable + " manage.py crontab add"
+            process_tasks_cmd = "python manage.py crontab add"
             process_tasks_subprocess = subprocess.Popen(process_tasks_cmd, env=os.environ.copy())
