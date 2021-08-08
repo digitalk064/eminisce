d35ec24ec1a94f2a6879f0f6d034aceecd88d419
digitalk064 <nguynkhanh.lv@gmail.com>
2021-08-08 10:12:28 +0700

    Prevent Heroku from running background tasks twice realsies this time.

--- eminisce/apps.py
+++ eminisce/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 import subprocess, os, sys
 import shlex
+import psutil
 
 class EminisceAppConfig(AppConfig):
     name = 'eminisce'
@@ -15,15 +16,47 @@ class EminisceAppConfig(AppConfig):
         #     # process_tasks_cmd = "python manage.py crontab add"
         #     # process_tasks_args = shlex.split(process_tasks_cmd)
         #     # process_tasks_subprocess = subprocess.Popen(process_tasks_args)
+        # UPDATE: Herooku does not have cron either :( and I'll die before I use their scheduler
 
         # UPDATE: Good news, background_tasks actually works here!
         # This is a really bad idea but it gets the job done
         print(f"{sys.argv} {os.environ.get('RUN_MAIN', None)}")
         if os.environ.get('RUN_MAIN', None) != 'true': # Don't execute on the autoreload thread           
-            if 'collectstatic' not in sys.argv and 'makemigrations' not in sys.argv and 'migrate' not in sys.argv and 'process_tasks' not in sys.argv: 
-                print("Starting background tasks")
-                from eminisce.tasks import auto_update_loans, cleanup_completed_tasks_db
-                print("Executing 'auto updating loans status' task")
-                auto_update_loans(repeat=1) # Repeat every second because why not
-                cleanup_completed_tasks_db(repeat=60) # For some reason this stupid plugin clutters up the database so we need to cleanup continuously
-                subprocess.Popen([sys.executable, 'manage.py', 'process_tasks'], env=os.environ.copy(),)
+            if 'collectstatic' not in sys.argv and 'makemigrations' not in sys.argv and 'migrate' not in sys.argv and 'process_tasks' not in sys.argv:
+                startTasks = True
+                
+                if (os.environ.get("DYNO")): # If running on heroku, make sure to only run on 1 process
+                    print("Running on Heroku, running process check")
+                    #check WEB_CONCURRENCY exists and is more than 1
+                    web_concurrency = os.environ.get("WEB_CONCURRENCY")
+                    if (web_concurrency):
+                        mypid = os.getpid()
+                        print("[%s] WEB_CONCURRENCY exists and is set to %s" % (mypid, web_concurrency))
+                        gunicorn_workers = int(web_concurrency)
+                        if (gunicorn_workers > 1):
+                            maxPid = self.getMaxRunningGunicornPid()
+                            if (maxPid == mypid):
+                                startTasks = True
+                            else:
+                                startTasks = False
+                
+                if startTasks:
+                    print("Starting background tasks")
+                    from eminisce.tasks import auto_update_loans, cleanup_completed_tasks_db
+                    print("Executing 'auto updating loans status' task")
+                    auto_update_loans(repeat=1) # Repeat every second because why not
+                    cleanup_completed_tasks_db(repeat=60) # For some reason this stupid plugin clutters up the database so we need to cleanup continuously
+                    subprocess.Popen([sys.executable, 'manage.py', 'process_tasks'], env=os.environ.copy(),)
+
+    # HEROKU/GUNICORN ONLY =================================================================================
+    def getMaxRunningGunicornPid(self):
+            running_pids = psutil.pids()
+            maxPid = -1
+            for pid in running_pids:
+                proc = psutil.Process(pid)
+                proc_name = proc.name()
+                if (proc_name == "gunicorn"):
+                    if (maxPid < pid):
+                        maxPid = pid
+            print("Max Gunicorn PID: %s", maxPid)
+            return maxPid
